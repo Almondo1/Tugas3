9c2321c2d022bf115c256c3819b657b87bd04135
Almondo Chen <pochen2345@gmail.com>
2022-09-30 20:32:54 +0700

    .editorconfig

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('profile');
+});
+Route::get('/experience', function () {
+    return view('experience');
+});
+Route::get('/education', function () {
+    return view('education');
+});
+Route::get('/certificate', function () {
+    return view('certificate');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/achievement', function () {
+    return view('achievement');
+});
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
